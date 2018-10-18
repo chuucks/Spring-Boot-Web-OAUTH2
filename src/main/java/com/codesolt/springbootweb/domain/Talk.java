e47e1c34d8b9cd8ad57a049c3625589762d06ebf
Carlos Salazar <carlos.salazar@codesolt.com>
2018-10-18 15:22:28 -0500

    Added a few more registers to DB

--- src/main/java/com/codesolt/springbootweb/domain/Talk.java
+++ src/main/java/com/codesolt/springbootweb/domain/Talk.java
@@ -1,23 +1,22 @@
 package com.codesolt.springbootweb.domain;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 @Entity
 public class Talk {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+	
 	@Column(name="EVENT_ID", updatable = false, nullable = false)
 	private Long eventId;
 	private String name;
